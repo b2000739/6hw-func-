53caa4a25c1f6cbb0c9d03e75ebe537bd72f60e5
b2000739 <113001869+b2000739@users.noreply.github.com>
2022-12-09 03:00:23 +0600

    Create index.php

---
+++ index.php
@@ -0,0 +1,26 @@
+<?php
+$c = 5;
+$d = 8;
+function sum($a, $b)
+{
+	return $a + $b;
+}
+echo sum($c, $d);
+echo '<br>';
+function minus($a, $b)
+{
+	return $a - $b;
+}
+echo minus($c, $d);
+echo '<br>';
+function multiply($a, $b)
+{
+	return $a * $b;
+}
+echo multiply($c, $d);
+echo '<br>';
+function division($a, $b)
+{
+	return $a / $b;
+}
+echo division($c, $d);
